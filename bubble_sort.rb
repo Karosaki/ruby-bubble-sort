ea2f2442c15b729365ea3b16eb50df1c0aee1787
Kai Stevens <kai.stevens97@gmail.com>
2022-03-01 15:18:39 +0000

    created the bubble sort, but in the unoptimised version, without the k-passes or checking for completion, currently will run through n elements

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,3 +4,27 @@
 # optimisations
 # for k passes you do not need to check n-(k+1)
 # check for sorted using a counter
+
+def bubble_sort(array)
+    for x in array # as many times as there are elements (not optimised yet)
+        array.each_index do |i|
+            # checking for nil
+            if array[i+1] == nil
+                next
+            end
+            # comparing element and element next to it
+            first = array[i]
+            second = array[i+1]
+            if first > second # if they are out of order, swap them
+                array[i] = second
+                array[i+1] = first
+            end
+        end
+
+        array
+    end
+end
+
+array = [9, 6, 5, 3, 2]
+bubble_sort(array)
+p array
