cdc5af7e460104124cf137ff519fb9e3a7cdb4b5
Kai Stevens <kai.stevens97@gmail.com>
2022-03-01 15:01:16 +0000

    added pseudocode for the algorith

---
+++ bubble_sort.rb
@@ -0,0 +1,6 @@
+# for each element in the list look at it [i] and the element directly next to it [i + 1]
+# if they are out of order, swap them
+
+# optimisations
+# for k passes you do not need to check n-(k+1)
+# check for sorted using a counter
